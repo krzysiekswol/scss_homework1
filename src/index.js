ca8611a992c320d5f74da5901a794abdcb0e1cfe
Krzysiek Swol <krzysiek.swol@gmail.com>
2021-01-23 11:43:51 +0100

    version 3.1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { sum } from "./sum";
+import style from "./css/index.css"
 console.log("Hello World");
 console.log(sum(2,3));
 
 
 let heading = document.querySelector("#demo"),
- sumValue = sum(10,5);
-heading.innerHTML = `10 + 5 = ${sumValue}`;
+ sumValue = sum(10,300);
+heading.innerHTML = `10 + 300 = ${sumValue}`;
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
+    },
+    devServer:{
+        contentBase: path.join(__dirname,"dist"),
+        port: 9000,
+        watchContentBase: true
+    },
+    module: {
+        rules: [{
+            test: /\.css$/, //what loaders should be applied
+            use: ["style-loader","css-loader"] //what loaders we want to
+        }]
     }
 }
