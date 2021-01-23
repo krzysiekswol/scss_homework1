e634e04e30a9d6f6bd3e17c1b06d34855634ee3d
Krzysiek Swol <krzysiek.swol@gmail.com>
2021-01-23 15:56:34 +0100

    version scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { sum } from "./sum";
-import style from "./css/index.scss"
+import style from "./css/index.scss";
 console.log("Hello World");
-console.log(sum(2,3));
-
+console.log(sum(2, 3));
 
 let heading = document.querySelector("#demo"),
- sumValue = sum(10,300);
+  sumValue = sum(10, 300);
 heading.innerHTML = `10 + 300 = ${sumValue}`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,38 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    devServer:{
-        contentBase: path.join(__dirname,"dist"),
-        port: 9000,
-        watchContentBase: true
-    },
-    // module: {
-    //     rules: [{
-    //         test: /\.css$/, //what loaders should be applied
-    //         use: ["style-loader/url","file-loader"] //what loaders we want to
-    //     }]
-    // },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"] //dopisanie komponentu
-               
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    watchContentBase: true,
+  },
+  // module: {
+  //     rules: [{
+  //         test: /\.css$/, //what loaders should be applied
+  //         use: ["style-loader/url","file-loader"] //what loaders we want to
+  //     }]
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"], //dopisanie komponentu
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-            loader: 'babel-loader',
-            options: {
-            presets: ['@babel/preset-env']}
-            }
-        }
-        
-
-    ]}
-}
+      },
+    ],
+  },
+};
