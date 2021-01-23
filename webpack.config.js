6be3ee46b566a37903a2e5ef360cfc6b0f01a05b
Krzysiek Swol <krzysiek.swol@gmail.com>
2021-01-23 12:12:33 +0100

    version 3.2.1

--- webpack.config.js
+++ webpack.config.js
@@ -11,19 +11,18 @@ module.exports = {
         port: 9000,
         watchContentBase: true
     },
-    module: {
-        rules: [{
-            test: /\.css$/, //what loaders should be applied
-            use: ["style-loader/url","file-loader"] //what loaders we want to
-        }]
-    },
+    // module: {
+    //     rules: [{
+    //         test: /\.css$/, //what loaders should be applied
+    //         use: ["style-loader/url","file-loader"] //what loaders we want to
+    //     }]
+    // },
     module: {
         rules: [{
             test: /\.css$/,
             use:  [
                 {loader: "style-loader/url"},
-                {loader: "file-loader",
-                options: {name: "[name].[ext]"}}
+                {loader: "file-loader", options: {name: "[name].[ext]"}}
             ]
                
         }]
